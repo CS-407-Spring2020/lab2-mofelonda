2fe54d87dd9c90706186640831144924177c20eb
Mo Felonda <felonda@wisc.edu>
2020-02-06 22:30:11 -0600

    Through activity 2

--- app/src/main/java/com/example/lab2/MainActivity.java
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -3,8 +3,18 @@ package com.example.lab2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void nameButtonClick(View view) {
+        EditText nameField = (EditText) findViewById(R.id.nameField);
+
+        Toast.makeText(MainActivity.this, nameField.getText().toString(), Toast.LENGTH_LONG).show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
